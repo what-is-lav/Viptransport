4b8c62f6213f0acc3d8e828887e283ed6c1727d6
uhuieee <153985079+uhuieee@users.noreply.github.com>
2025-12-21 17:38:33 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 3000;
 const frontendPath = path.join(__dirname, '..', 'frontend', 'viptransport');
 
 app.use(express.static(frontendPath));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/photo', express.static(path.join(frontendPath, 'photo')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(frontendPath, 'home.html'));
 });
 
-app.get('/:page', (req, res) => {
+app.get('/:page([a-zA-Z0-9-]+)', (req, res) => {
   res.sendFile(path.join(frontendPath, `${req.params.page}.html`));
 });
 
